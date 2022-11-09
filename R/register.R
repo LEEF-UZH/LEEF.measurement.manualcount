8e80c49f7c473e8bf9c85c7c3415e8c8e41a3768
Rainer M. Krug <Rainer@krugs.de>
2022-11-09 15:08:42 +0100

    Renamed to LEEF.2

--- R/register.R
+++ R/register.R
@@ -5,12 +5,12 @@
 #' @export
 #'
 register <- function() {
-  if (is.null(system.file(package = "LEEF"))) {
+  if (is.null(system.file(package = "LEEF.2"))) {
     stop("This function requres the package to be installed!")
   }
 
-  LEEF::add_pre_processor( pre_processor_manualcount )
-  LEEF::add_extractor( extractor_manualcount )
+  LEEF.2::add_pre_processor( pre_processor_manualcount )
+  LEEF.2::add_extractor( extractor_manualcount )
   ##
   invisible(TRUE)
 }
